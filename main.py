b171f5baa96e7d429d71850b4ece938c1fe260f4
Maylan <maylan.gomes@epitech.eu>
2024-06-11 13:26:17 +0200

    flask

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
 def hello_world():
-    print("hello world")
+    return 'Hello, World!'
 
 if __name__ == "__main__":
-    hello_world()
+    app.run(host='0.0.0.0', port=8000)
